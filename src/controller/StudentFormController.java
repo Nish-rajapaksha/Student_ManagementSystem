1f676cd3f7554a587e6fa058bb749259e647dee3
Nishadi rajapaksha <93418323+Nish-rajapaksha@users.noreply.github.com>
2022-07-12 11:54:14 +0530

    Update,Save,search,Load all

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
@@ -11,6 +14,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import model.Student;
 import util.CrudUtil;
+import views.StudentTm;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,7 +24,7 @@ public class StudentFormController {
     public JFXTextField txtName;
     public JFXTextField txtAddress;
     public JFXTextField txtContact;
-    public TableView tblStudent;
+    public TableView <StudentTm> tblStudent;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colEmail;
@@ -30,6 +34,8 @@ public class StudentFormController {
     public JFXTextField txtId;
     public JFXTextField txtNIC;
     public JFXTextField txtEmail;
+    public JFXTextField txtSearch;
+    public JFXButton btnAdd;
 
     public void initialize(){
 
@@ -42,57 +48,141 @@ public class StudentFormController {
         colNic.setCellValueFactory(new PropertyValueFactory("nic"));
 
         try {
-            loadAllCustomers();
+            loadAllStudent();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            btnAdd.setText(newValue != null ? "Update" : "Add");
+            txtId.setEditable(false);
+            if (newValue != null) {
+                txtId.setText(newValue.getStudentId());
+                txtName.setText(newValue.getStudentName());
+                txtEmail.setText(newValue.getEmail());
+                txtContact.setText(newValue.getContact());
+                txtAddress.setText(newValue.getAddress());
+                txtNIC.setText(newValue.getNic());
+            }
+        });
+
+
 
     }
 
 
-    private void loadAllCustomers() throws ClassNotFoundException, SQLException {
-        ResultSet result = CrudUtil.execute("SELECT * FROM Student");
-        ObservableList<Student> obList = FXCollections.observableArrayList();
+    private void loadAllStudent() throws SQLException, ClassNotFoundException {
 
-        while (result.next()){
+        ResultSet result = CrudUtil.execute("SELECT * FROM Student");
+        ObservableList<StudentTm> obList = FXCollections.observableArrayList();
+
+        while (result.next()) {
             obList.add(
-                    new Student(
-                            result.getString("studentId"),
-                            result.getString("studentName"),
+                    new StudentTm(
+                            result.getString("student_id"),
+                            result.getString("student_Name"),
                             result.getString("email"),
                             result.getString("contact"),
                             result.getString("address"),
                             result.getString("nic")
-
                     )
             );
         }
         tblStudent.setItems(obList);
 
+
+        FilteredList<StudentTm> filterData = new FilteredList<StudentTm>(obList, b -> true);
+
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterData.setPredicate(Student -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+
+
+                if (Student.getStudentId().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                } else return Student.getStudentName().toLowerCase().indexOf(lowerCaseFilter) != -1;
+
+            });
+        });
+
+        SortedList<StudentTm> sortedData = new SortedList<>(filterData);
+
+        sortedData.comparatorProperty().bind(tblStudent.comparatorProperty());
+
+        tblStudent.setItems(sortedData);
+
     }
 
     public void addOnAction(ActionEvent actionEvent) {
-        Student s = new Student(
-                txtId.getText(), txtName.getText(), txtEmail.getText(),txtContact.getText(),txtAddress.getText(),txtNIC.getText());
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String email = txtEmail.getText();
+        String contact = txtContact.getText();
+        String address = txtAddress.getText();
+        String nic = txtNIC.getText();
+
+
+        if (btnAdd.getText().equals("Add")) {
+            Student s = new Student(
+                    txtId.getText(), txtName.getText(), txtEmail.getText(), txtContact.getText(), txtAddress.getText(), txtNIC.getText());
+
+            try {
+
+
+                if (CrudUtil.execute("INSERT INTO Student VALUES (?,?,?,?,?,?)", s.getStudentId(), s.getStudentName(), s.getEmail(), s.getContact(), s.getAddress(), s.getNic())) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Saved Student!..").show();
+                }
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            }
+
+        } else {
+            try {
+                CrudUtil.execute("UPDATE Student SET student_name=?,email=?,contact=?,address=?,nic=? where student_id=?", name, email, contact, address, nic, id);
+                new Alert(Alert.AlertType.CONFIRMATION, "Updated Student!..").show();
+
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            StudentTm selectedStudent = tblStudent.getSelectionModel().getSelectedItem();
+            selectedStudent.setStudentName(name);
+            selectedStudent.setEmail(email);
+            selectedStudent.setContact(contact);
+            selectedStudent.setAddress(address);
+            selectedStudent.setNic(nic);
+
+        }
+
+        StudentTm selectedStudent = tblStudent.getSelectionModel().getSelectedItem();
+        selectedStudent.setStudentName(name);
+        selectedStudent.setEmail(email);
+        selectedStudent.setContact(contact);
+        selectedStudent.setAddress(address);
+        selectedStudent.setNic(nic);
+
 
         try {
-
-
-            if (CrudUtil.execute("INSERT INTO Student VALUES (?,?,?,?,?,?)",s.getStudentId(),s.getStudentName(),s.getEmail(),s.getContact(),s.getAddress(),s.getNic())) {
-                new Alert(Alert.AlertType.CONFIRMATION, "Saved Student!..").show();
-            }
-        } catch (ClassNotFoundException | SQLException e) {
+            loadAllStudent();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
+
     }
+
 
     public void newOnAction(ActionEvent actionEvent) {
     }
 
     public void deleteOnAction(ActionEvent actionEvent) {
+
     }
 }
--- src/views/StudentTm.java
+++ src/views/StudentTm.java
@@ -1,19 +1,84 @@
 package views;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
+
 public class StudentTm {
-    private String studentId;
-    private String studentName;
-    private String email;
-    private String contact;
-    private String address;
+     private String studentId;
+     private String studentName;
+     private String email;
+     private String contact;
+     private String address;
     private String nic;
+
+    public StudentTm() {
+    }
+
+    public StudentTm(String studentId, String studentName, String email, String contact, String address, String nic) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.email = email;
+        this.contact = contact;
+        this.address = address;
+        this.nic = nic;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTm{" +
+                "studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", email='" + email + '\'' +
+                ", contact='" + contact + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }
